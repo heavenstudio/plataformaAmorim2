cb38c7e32b70e75876f2e469bcc354c7412628ea
Diego Garcia <diegomg0301@gmail.com>
2015-12-09 11:07:36 -0200

    Mudança Buscar Mural Range

--- plataformaAmorim/src/br/com/muranodesign/resources/MuralResource.java
+++ plataformaAmorim/src/br/com/muranodesign/resources/MuralResource.java
@@ -17,11 +17,11 @@ import javax.ws.rs.Produces;
 
 import org.apache.log4j.Logger;
 
-import br.com.muranodesign.business.AgrupamentoService;
 import br.com.muranodesign.business.AlunoAgrupamentoService;
 import br.com.muranodesign.business.AlunoVariavelService;
 import br.com.muranodesign.business.AnoEstudoService;
 import br.com.muranodesign.business.GrupoService;
+import br.com.muranodesign.business.MuralAlunoService;
 import br.com.muranodesign.business.MuralService;
 import br.com.muranodesign.business.ProfessorFuncionarioService;
 import br.com.muranodesign.business.RotinaService;
@@ -31,6 +31,7 @@ import br.com.muranodesign.model.AlunoAgrupamento;
 import br.com.muranodesign.model.AlunoVariavel;
 import br.com.muranodesign.model.Grupo;
 import br.com.muranodesign.model.Mural;
+import br.com.muranodesign.model.MuralAluno;
 import br.com.muranodesign.model.ProfessorFuncionario;
 import br.com.muranodesign.model.Rotina;
 import br.com.muranodesign.model.Tutoria;
@@ -62,9 +63,10 @@ public class MuralResource {
 			@FormParam("id") int id,
 			@FormParam("mensagem") String mensagem,
 			@FormParam("idProfessor") int idProfessor,
-			@FormParam("idAgrupamento") int idAgrupamento,
-			@FormParam("idOficina") int idOficina,
-			@FormParam("tutor") int tutor,
+			@FormParam("agrupamento") int idAgrupamento,
+			@FormParam("oficina") int idOficina,
+			@FormParam("tutoria") int tutor,
+			@FormParam("grupo") int grupo,
 			@FormParam("ano") String ano,
 			@FormParam("idAluno") int idAluno) throws ParseException{
 		
@@ -83,164 +85,102 @@ public class MuralResource {
 			String dataS = formatter.format(dataT);
 			
 			Date data = (Date) formatter.parse(dataS);
-		
+			
 			
 			Mural mural = new Mural();
-			
+			mural.setMensagem(mensagem);
+			mural.setData(data);
+			mural.setProfessor(new ProfessorFuncionarioService().listarkey(idProfessor).get(0));
+			new MuralService().criarMural(mural);
 			
 			ProfessorFuncionario verificarCoordena = new ProfessorFuncionarioService().listarkey(idProfessor).get(0);
 			if(verificarCoordena.getPerfil() == 26){
 				
-				  String [] arrayAnos = ano.split(";");
 				  List<AlunoVariavel> listAluno = new ArrayList<AlunoVariavel>();
-				  
-				  
-				  for(int j = 0; j < arrayAnos.length; j++){
-					  listAluno.addAll(new AlunoVariavelService().listaAnoEstudo(new AnoEstudoService().listarkey(Integer.parseInt(arrayAnos[j])).get(0)));
-					  
-					  
-				  }
+				  listAluno.addAll(new AlunoVariavelService().listaAnoEstudo(new AnoEstudoService().listarkey(Integer.parseInt(ano)).get(0)));			  
 				  
 				  for(int k = 0; k < listAluno.size(); k++){
-					mural.setMensagem(mensagem);
-					mural.setAluno(listAluno.get(k));
-					mural.setData(data);
-					
-					mural.setProfessor(new ProfessorFuncionarioService().listarkey(idProfessor).get(0));
-					
-					new MuralService().atualizarMural(mural);
+					  MuralAluno muralAluno = new MuralAluno();
+					  muralAluno.setAlunoVariavel(listAluno.get(k));
+					  muralAluno.setMural(mural);
+					  new MuralAlunoService().criarMuralAluno(muralAluno);
 				  }
 				
 				  return "ok";
 				
-			}else {
-			
-			if(tutor == 1  && idOficina == 0){
-				Tutoria profTutor = new TutoriaService().listarProfessorId(idProfessor).get(0);
+			}
+			else {
+				if (tutor != 0){
+					Tutoria profTutor = new TutoriaService().listarProfessorId(idProfessor).get(0);
 				
-				List<Grupo> grupos = new GrupoService().listarTutor(profTutor.getIdtutoria());
+					List<Grupo> grupos = new GrupoService().listarTutor(profTutor.getIdtutoria());
 				
-				List<AlunoVariavel> alunos = new ArrayList<AlunoVariavel>();
+					List<AlunoVariavel> alunos = new ArrayList<AlunoVariavel>();
 				
-				for (Grupo grupo : grupos) {
-					alunos.addAll(new AlunoVariavelService().listaGrupo(grupo.getIdgrupo()));
+					for (Grupo g : grupos) {
+						alunos.addAll(new AlunoVariavelService().listaGrupo(g.getIdgrupo()));
+					}
+				
+					for (AlunoVariavel alunoVariavel : alunos) {
+						MuralAluno muralAluno = new MuralAluno();
+						muralAluno.setAlunoVariavel(alunoVariavel);
+						muralAluno.setMural(mural);
+						new MuralAlunoService().criarMuralAluno(muralAluno);
+					}
+				
+					return "ok";
+				
 				}
+				else if(grupo != 0){
+					List<AlunoVariavel> alunos = new ArrayList<AlunoVariavel>();
+					alunos.addAll(new AlunoVariavelService().listaGrupo(grupo));
 				
-				for (AlunoVariavel alunoVariavel : alunos) {
-					
-					mural.setMensagem(mensagem);
-					mural.setAluno(alunoVariavel);
-					mural.setData(data);
-		
-					mural.setProfessor(new ProfessorFuncionarioService().listarkey(idProfessor).get(0));
+					for (AlunoVariavel alunoVariavel : alunos) {
+						MuralAluno muralAluno = new MuralAluno();
+						muralAluno.setAlunoVariavel(alunoVariavel);
+						muralAluno.setMural(mural);
+						new MuralAlunoService().criarMuralAluno(muralAluno);
+					}
 				
-					new MuralService().atualizarMural(mural);
+					return "ok";
 				}
-				
-				return "ok";
-				
-			}else if(tutor == 0 && idOficina != 0){
-				List<Rotina> rotina = new RotinaService().listarPorOficina(idOficina);
-				
-				List<Agrupamento> agrupamento = new ArrayList<Agrupamento>();
-				
-				List<AlunoAgrupamento> alunoAgrupamento = new ArrayList<AlunoAgrupamento>();
-
-				List<AlunoVariavel> alunos = new ArrayList<AlunoVariavel>();
-				
-				
-				for (Rotina rotina2 : rotina) {
-					agrupamento.addAll(new AgrupamentoService().listarkey(rotina2.getAgrupamento().getIdagrupamento()));
-				}
-				
-				for (Agrupamento agrupamento2 : agrupamento) {
-					
-					alunoAgrupamento.addAll(new AlunoAgrupamentoService().listarAgrupamento(agrupamento2.getIdagrupamento()));
-				}
-				
-				for (AlunoAgrupamento alunoAgrupamento2 : alunoAgrupamento) {
-					
-					alunos.addAll(new AlunoVariavelService().listarkey(alunoAgrupamento2.getAluno().getIdalunoVariavel()));
-				}
-				
-				for (AlunoVariavel alunoVariavel : alunos) {
-					
-					mural.setMensagem(mensagem);
-					mural.setAluno(alunoVariavel);
-					mural.setData(data);
-			
-					mural.setProfessor(new ProfessorFuncionarioService().listarkey(idProfessor).get(0));
-					
-					new MuralService().criarMural(mural);
-
-				}
-				
-				return "ok";
-				
-			}else if(tutor == 1 && idOficina != 0){
-				Tutoria profTutor = new TutoriaService().listarProfessorId(idProfessor).get(0);
-				
-				List<Grupo> grupos = new GrupoService().listarTutor(profTutor.getIdtutoria());
-				
-				List<Rotina> rotina = new RotinaService().listarPorOficina(idOficina);
-				
-				List<Agrupamento> agrupamento = new ArrayList<Agrupamento>();
-				
-				List<AlunoAgrupamento> alunoAgrupamento = new ArrayList<AlunoAgrupamento>();
-				
-				List<AlunoVariavel> alunosComTutor = new ArrayList<AlunoVariavel>();
-				List<AlunoVariavel> alunosComOficina = new ArrayList<AlunoVariavel>();
-				List<AlunoVariavel> alunosConsolidados = new ArrayList<AlunoVariavel>();
-				
-				
-				for (Grupo grupo : grupos) {
-					alunosComTutor.addAll(new AlunoVariavelService().listaGrupo(grupo.getIdgrupo()));
-				}
-				
-				
-				for (Rotina rotina2 : rotina) {
-					agrupamento.addAll(new AgrupamentoService().listarkey(rotina2.getAgrupamento().getIdagrupamento()));
-				}
-				
-				for (Agrupamento agrupamento2 : agrupamento) {
-					
-					alunoAgrupamento.addAll(new AlunoAgrupamentoService().listarAgrupamento(agrupamento2.getIdagrupamento()));
-				}
-				
-				for (AlunoAgrupamento alunoAgrupamento2 : alunoAgrupamento) {
-					
-					alunosComOficina.addAll(new AlunoVariavelService().listarkey(alunoAgrupamento2.getAluno().getIdalunoVariavel()));
-				}
-				
-				for(int i = 0; i < alunosComTutor.size(); i++){
-					if(!alunosConsolidados.contains(alunosComTutor.get(i))){
-						mural.setMensagem(mensagem);
-						mural.setAluno(alunosComTutor.get(i));
-						mural.setData(data);
-					
-						mural.setProfessor(new ProfessorFuncionarioService().listarkey(idProfessor).get(0));
-						
-						new MuralService().criarMural(mural);
+				if (idOficina != 0){
+					List<AlunoVariavel> alunos = new ArrayList<AlunoVariavel>();
+					List<AlunoAgrupamento> alunoAgrupamentos = new ArrayList<AlunoAgrupamento>();
+					List<Agrupamento> agrupamentos = new ArrayList<Agrupamento>();
+					List<Rotina> rotinas = new RotinaService().listarPorOficina(idOficina);
+					for (Rotina rotina : rotinas) {
+						agrupamentos.add(rotina.getAgrupamento());
+					}
+					for (Agrupamento agrupamento : agrupamentos) {
+						alunoAgrupamentos.addAll(new AlunoAgrupamentoService().listarAgrupamento(agrupamento.getIdagrupamento()));
+					}
+					for (AlunoAgrupamento alunoAgrupamento : alunoAgrupamentos) {
+						alunos.add(alunoAgrupamento.getAluno());
+					}
+					for (AlunoVariavel a: alunos) {
+						MuralAluno muralAluno = new MuralAluno();
+						muralAluno.setAlunoVariavel(a);
+						muralAluno.setMural(mural);
+						new MuralAlunoService().criarMuralAluno(muralAluno);
 					}
 				}
-				
-				for(int j = 0; j < alunosComOficina.size(); j++){
-					if(!alunosConsolidados.contains(alunosComOficina.get(j))){
-						mural.setMensagem(mensagem);
-						mural.setAluno(alunosComOficina.get(j));
-						mural.setData(data);
-			
-						mural.setProfessor(new ProfessorFuncionarioService().listarkey(idProfessor).get(0));
-						
-						new MuralService().criarMural(mural);
+				else if(idAgrupamento != 0){
+					List<AlunoVariavel> alunos = new ArrayList<AlunoVariavel>();
+					List<AlunoAgrupamento> alunoAgrupamento = new ArrayList<AlunoAgrupamento>();
+					alunoAgrupamento = new AlunoAgrupamentoService().listarAgrupamento(idAgrupamento);
+					for (AlunoAgrupamento a : alunoAgrupamento) {
+						alunos.add(a.getAluno());
+					}
+					for (AlunoVariavel a: alunos) {
+						MuralAluno muralAluno = new MuralAluno();
+						muralAluno.setAlunoVariavel(a);
+						muralAluno.setMural(mural);
+						new MuralAlunoService().criarMuralAluno(muralAluno);
 					}
 				}
-				
-				return "ok";
 			}
 		}
-		}
-		
 		return "erro";
 	}
 	
@@ -258,16 +198,16 @@ public class MuralResource {
 		 logger.debug("QTD Mural : " +  resultado.size());
 		return resultado;
 	}
-		
+	
 	/**
-	 * Listar por range de data
-	 * @param idAluno
+	 * Listar por professor
+	 * @param idProfessor
 	 * @return
 	 */
-	@Path("RangeData/{idAluno}")
+	@Path("ListarProfessor/{idProfessor}")
 	@GET
 	@Produces("application/json")
-	public List<Mural> getRange(@PathParam("idAluno") int idAluno){
+	public List<Mural> getRangeProfessor(@PathParam("idProfessor") int idProfessor){
 		Date dataHoje = new Date();
 		SimpleDateFormat formataData = new SimpleDateFormat("yy-MM-dd");
 		String data = formataData.format(dataHoje);
@@ -298,8 +238,50 @@ public class MuralResource {
 			
 		}
 		
-		return new MuralService().Range(data2,data,idAluno);
+		return new MuralService().Range(data2,data,idProfessor);
 	}
+		
+	/**
+	 * Listar por range de data
+	 * @param idAluno
+	 * @return
+	 */
+	/*@Path("ListarAluno/{idAluno}")
+	@GET
+	@Produces("application/json")
+	public List<Mural> getRangeAluno(@PathParam("idAluno") int idAluno){
+		Date dataHoje = new Date();
+		SimpleDateFormat formataData = new SimpleDateFormat("yy-MM-dd");
+		String data = formataData.format(dataHoje);
+		
+		Calendar c = Calendar.getInstance();
+		int mes = c.get(Calendar.MONTH);
+		String data2;
+		if(mes > 3){
+			mes = mes - 3;
+			String quebra[] = data.split("-");
+			
+			if(mes > 10){
+				data2 = quebra[0]+"-"+Integer.toString(mes)+"-"+quebra[2];
+			}else{
+				data2 = quebra[0]+"-"+"0"+Integer.toString(mes)+"-"+quebra[2];
+			}
+			
+		}else{
+			int aux = 12;
+			mes = mes - 3;
+			aux = aux + mes;
+			
+			String quebra[] = data.split("-");
+			int ano = Integer.parseInt(quebra[0]);
+			ano = ano - 1;
+			
+			data2 = Integer.toString(ano)+"-"+Integer.toString(aux)+"-"+quebra[2];
+			
+		}
+		
+		return new MuralService().RangeAluno(data2,data,idAluno);
+	}*/
 	
 		
 		
