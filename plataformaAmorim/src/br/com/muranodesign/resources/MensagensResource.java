652e44b498c5f09ea600e629479ec3f2347cea02
Diego Garcia <diegomg0301@gmail.com>
2016-05-03 19:01:38 -0300

    Fix Mensagem

--- plataformaAmorim/src/br/com/muranodesign/resources/MensagensResource.java
+++ plataformaAmorim/src/br/com/muranodesign/resources/MensagensResource.java
@@ -191,7 +191,7 @@ public class MensagensResource {
 				aux.put("remetente_perfil", "Aluno");
 				aux.put("remetente_nome", mensagens.getRemetente().getAluno().getNome());	
 				
-			}else if (mensagens.getRemetente().getPerfil().getIdperfil() == 24){
+			}else {
 				aux.put("remetente_perfil", "Professor");
 				aux.put("remetente_nome", mensagens.getRemetente().getProfessor().getNome());		
 			}
